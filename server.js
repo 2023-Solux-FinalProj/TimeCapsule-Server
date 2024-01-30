04eef1f9f75d0577b122bd06063f837bda645f68
molcham <130418659+molcham@users.noreply.github.com>
2024-01-30 21:48:32 +0900

    /capsule 노중첩데이터

--- server.js
+++ server.js
@@ -72,10 +72,6 @@ app.get('/', function(req, res){
 
 
 
-// /send 응답 
-// app.get('/capsule', (req, res) => {
-// 	res.send('캡슐이 전송 되었습니다!---정보저장중!');
-// });
 
 // 여기까지 git pull 코드
 
@@ -273,26 +269,20 @@ app.post('/capsule',
 }, 
     
 (req, res) => {
-	const receiver = req.body.receiver;
-        const capsule = req.body.capsule; // "capsule" 객체 추출
-	const writer = capsule.writer;
-	const writtendate = capsule.writtendate;
-	const arrivaldate = capsule.arrivaldate;
-	const cards = capsule.cards;
-	const music = capsule.music;
-	const theme = capsule.theme;
-	
-	// arrivaldate의 값 추출
-	const year = arrivaldate.year;
-	const month = arrivaldate.month;
-	const day = arrivaldate.day;
-	
-	// 날짜 문자열로 변환
-	const arrivalDateString = `${year}-${month}-${day}`;
-	const send_at = writtendate;
-	const arrive_at = new Date(arrivalDateString);
-
-    
+    const receiver = req.body.receiver;
+    const writer = req.body.writer;
+    const writtendate = req.body.writtendate;
+    const arrivaldate = {
+        year: req.body['arrivaldate[year]'],
+        month: req.body['arrivaldate[month]'],
+        day: req.body['arrivaldate[day]']
+    };
+    const cards = req.body;   
+    const music=req.body.music;
+    const theme=req.body.theme;
+    const arrivalDateString = `${arrivaldate.year}-${arrivaldate.month}-${arrivaldate.day}`;
+    const send_at = writtendate;
+    const arrive_at = new Date(arrivalDateString);
 
     const getWriterIDQuery = 'SELECT memberID FROM User WHERE username = ?';
     connection.query(getWriterIDQuery, [writer], (err, userResult) => {
@@ -376,15 +366,6 @@ function saveImage(base64Data) {
     fs.writeFileSync(imagePath, imageBuffer); 
     return imagePath;
 }
-
-
-
-   
-     
-
-
-
-
 
 
 	
